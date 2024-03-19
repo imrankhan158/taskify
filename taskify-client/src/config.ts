596eace7e088dda8035cc9988efb9da212a23a14
Imran Khan <imran158khan@gmail.com>
2024-03-20 03:37:46 +0530

    Fix NGINX port issue

--- taskify-client/src/config.ts
+++ taskify-client/src/config.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = import.meta.env.VITE__SERVER_BASE_URL;
-export const HOST_NAME = import.meta.env.VITE__SERVER_HOSTNAME;
+export const BASE_URL = import.meta.env.VITE_SERVER_BASE_URL;
+export const HOST_NAME = import.meta.env.VITE_SERVER_HOSTNAME;
